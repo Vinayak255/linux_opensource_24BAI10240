48c021f0083f9fe70d549dc17ede5920b0dd7980
Vinayak255 <117843181+Vinayak255@users.noreply.github.com>
2026-03-31 16:30:10 +0530

    Update script2_package_inspector.sh

--- script2_package_inspector.sh
+++ script2_package_inspector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # script2_package_inspector.sh
-# Author: Pradyumn Krishna Arya
-# Roll Number: 24bai10668
+# Author:Vinayak Bhadauria
+# Roll Number: 24BAI10240
 # Purpose: Check if Git is installed and show details about some FOSS tools
 
 echo "==========================================="
